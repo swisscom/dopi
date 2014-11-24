6b4aaf408c7747c4bf580e24fce8e57e87172437
Andreas Zuber <zuber@puzzle.ch>
2014-11-24 18:07:09 +0100

    Cli tests

--- lib/dopi.rb
+++ lib/dopi.rb
@@ -1,4 +1,9 @@
+require "dopi/cli"
+require "dopi/configure"
+require "dopi/node"
+require "dopi/plan"
 require "dopi/version"
+
 
 module Dopi
   # Your code goes here...
--- lib/dopi/cli.rb
+++ lib/dopi/cli.rb
@@ -1,12 +1,20 @@
 #
 # Dopi cli
 #
-require 'dopi'
 require 'thor'
 
-class Dopi::Cli < Thor
-  desc "hello NAME", "say hello to NAME"
-  def hello(name)
-    puts "Hello #{name}"
+module Dopi
+
+  class Cli < Thor
+    desc "nodes PLAN", "Lists all nodes in the plan"
+    options Dopi.configuration.instance_variables
+    def nodes(plan)
+      Dopi.configure {}
+      plan = Plan.new( File.read( plan ) )
+      plan.nodes.each do |node|
+        puts node.fqdn + ' : ' + node.role
+      end
+    end
   end
+
 end
