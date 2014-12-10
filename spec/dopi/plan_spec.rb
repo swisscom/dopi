696faa329f48a7d3608f76ebb3d9b981df0c2437
Andreas Zuber <zuber@puzzle.ch>
2014-12-10 18:31:17 +0100

    tests for plan class

--- spec/dopi/plan_spec.rb
+++ spec/dopi/plan_spec.rb
@@ -15,23 +15,25 @@ describe Dopi::Plan do
     @plan = Dopi::Plan.new( File.read( plan_yaml ) )
   end
 
-  describe "#new" do
-
-    it "takes a plan yaml file and returns a Dopi::Plan object" do
+  describe '#new' do
+    it 'takes a plan yaml file and returns a Dopi::Plan object' do
       expect(@plan).to be_an_instance_of Dopi::Plan
     end
-
-    it "creates 5 nodes" do
-      expect(@plan.nodes.length).to be 5
-    end
-
-    it "creates 2 steps" do
-      expect(@plan.steps.length).to be 2
-    end
-
   end
 
-  describe "#run" do
+  describe '#nodes' do
+    it 'creates 5 nodes' do
+      expect(@plan.nodes.length).to be 5
+    end
+  end
+
+  describe '#steps' do
+    it 'creates 2 steps' do
+      expect(@plan.steps.length).to be 2
+    end
+  end
+
+  describe '#run' do
     pending
   end
 
