b4012c1e55f3f2d07b400a9c5b99fa0cb43fef87
Andreas Zuber <zuber@puzzle.ch>
2014-12-10 14:55:50 +0100

    tests for plan class

---
+++ spec/dopi/plan_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Dopi::Plan do
+
+  before :all do
+    Dopi.log.level = ::Logger::ERROR
+
+    Dopi.configure do |config|
+      config.role_variable = 'my_role'
+    end
+  end
+
+  before :each do
+    plan_yaml = 'spec/data/plan/plan_simple.yaml'
+    @plan = Dopi::Plan.new( File.read( plan_yaml ) )
+  end
+
+  describe "#new" do
+
+    it "takes a plan yaml file and returns a Dopi::Plan object" do
+      expect(@plan).to be_an_instance_of Dopi::Plan
+    end
+
+    it "creates 5 nodes" do
+      expect(@plan.nodes.length).to be 5
+    end
+
+    it "creates 2 steps" do
+      expect(@plan.steps.length).to be 2
+    end
+
+  end
+
+  describe "#run" do
+    pending
+  end
+
+end
+
