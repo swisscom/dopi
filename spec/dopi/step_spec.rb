c7c4ce4804b0b5f902c032405d0a36ebec23159f
Andreas Zuber <zuber@puzzle.ch>
2015-05-20 14:53:29 +0200

    fix step tests

--- spec/dopi/step_spec.rb
+++ spec/dopi/step_spec.rb
@@ -7,6 +7,10 @@ describe Dopi::Step do
   end
 
   before :each do
+    plan_file = 'spec/data/plan/plan_simple.yaml'
+    plan_parser = DopCommon::Plan.new(YAML.load_file(plan_file))
+    @plan = Dopi::Plan.new(plan_parser, 'fakeid')
+
     hash = {
       :name      => 'Test step',
       :command   => {
@@ -15,10 +19,7 @@ describe Dopi::Step do
         :arguments => 'Hello World'
       }
     }
-    @nodes = []
-    @nodes << Dopi::Node.new(DopCommon::Node.new('web01.example.com', {'role' => 'role1'}))
-    @nodes << Dopi::Node.new(DopCommon::Node.new('web02.example.com', {'role' => 'role2'}))
-    @step = Dopi::Step.new(DopCommon::Step.new(hash), @nodes)
+    @step = Dopi::Step.new(DopCommon::Step.new(hash), @plan.nodes)
   end
 
   describe '#new' do
@@ -27,7 +28,7 @@ describe Dopi::Step do
     end
 
     it 'takes a name, a plugin name, a list of nodes and returns a Dopi::Step object' do
-      step = Dopi::Step.new(DopCommon::Step.new({:name => 'dummy', :command => 'dummy'}), @nodes)
+      step = Dopi::Step.new(DopCommon::Step.new({:name => 'dummy', :command => 'dummy'}), @plan.nodes)
       expect(step).to be_an_instance_of Dopi::Step
     end
   end
