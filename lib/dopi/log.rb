7028efaed6a31ebce4d8e912db1cfe4c36074eeb
Andreas Zuber <zuber@puzzle.ch>
2015-06-03 16:13:21 +0200

    Set dop_common logger when setting dopi logger

--- lib/dopi/log.rb
+++ lib/dopi/log.rb
@@ -2,15 +2,17 @@
 # the logger stuff
 #
 require 'logger'
+require 'dop_common'
 
 module Dopi
 
   def self.log
-    @log ||= Logger.new(STDOUT)
+    @log ||= Dopi.logger(Logger.new(STDOUT))
   end
 
   def self.logger=(logger)
     @log = logger
+    DopCommon.logger = logger
   end
 
 end
