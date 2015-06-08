a774fa3550ebc4d3063b41f50cb7a31cde0efe19
Andreas Zuber <zuber@puzzle.ch>
2015-06-08 20:48:18 +0200

    Tests for expect_exit_codes method in custom plugin

--- spec/dopi/command/custom_spec.rb
+++ spec/dopi/command/custom_spec.rb
@@ -56,7 +56,37 @@ describe Dopi::Command do
   end
 
   describe '#expect_exit_codes' do
-    pending
+    it 'should return 0 if nothing is specified' do
+      command_parser = DopCommon::Command.new({:plugin => 'custom'})
+      command = Dopi::Command.create_plugin_instance(command_parser.plugin, @node, command_parser)
+      expect(command.expect_exit_codes).to eq(0)
+    end
+    it 'should return a number if a number is specified' do
+      command_parser = DopCommon::Command.new({:plugin => 'custom', :expect_exit_codes => 3})
+      command = Dopi::Command.create_plugin_instance(command_parser.plugin, @node, command_parser)
+      expect(command.expect_exit_codes).to eq(3)
+    end
+    it 'should return an Array of numbers if such an Array is specified' do
+      command_parser = DopCommon::Command.new({:plugin => 'custom', :expect_exit_codes => [0, 1, 3]})
+      command = Dopi::Command.create_plugin_instance(command_parser.plugin, @node, command_parser)
+      expect(command.expect_exit_codes).to eq([0, 1, 3])
+    end
+    it 'should return "all" if such a String is specified' do
+      command_parser = DopCommon::Command.new({:plugin => 'custom', :expect_exit_codes => 'all'})
+      command = Dopi::Command.create_plugin_instance(command_parser.plugin, @node, command_parser)
+      expect(command.expect_exit_codes).to eq('all')
+    end
+    it 'will raise and error if the value is an invalid string' do
+      command_parser = DopCommon::Command.new({:plugin => 'custom', :expect_exit_codes => 'foo'})
+      command = Dopi::Command.create_plugin_instance(command_parser.plugin, @node, command_parser)
+      expect{command.expect_exit_codes}.to raise_error Dopi::CommandParsingError
+    end
+    it 'will raise and error if the Array contains a String' do
+      command_parser = DopCommon::Command.new({:plugin => 'custom', :expect_exit_codes => [1, 2, 'foo']})
+      command = Dopi::Command.create_plugin_instance(command_parser.plugin, @node, command_parser)
+      expect{command.expect_exit_codes}.to raise_error Dopi::CommandParsingError
+    end
+
   end
 
 end
