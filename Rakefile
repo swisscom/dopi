981708052e07eef02605a4b4eb6e105a410313b8
Andreas Zuber <zuber@puzzle.ch>
2015-06-17 13:47:37 +0200

    split integration tests from unit tests

--- Rakefile
+++ Rakefile
@@ -3,6 +3,17 @@ require 'rspec/core/rake_task'
 
 RSpec::Core::RakeTask.new('spec')
 
+namespace :spec do
+  RSpec::Core::RakeTask.new(:unit) do |t|
+    t.pattern = 'spec/unit/**/*_spec.rb'
+  end
+
+  RSpec::Core::RakeTask.new(:integration) do |t|
+    t.pattern = 'spec/integration/**/*_spec.rb'
+  end
+
+end
+
 task :default => :spec
 task :test => :spec
 
--- spec/unit/dopi/command_spec.rb
+++ spec/unit/dopi/command_spec.rb
@@ -20,20 +20,4 @@ describe Dopi::Command do
     end
   end
 
-  # Create plugin tests from plugin test yaml files
-  Dir['spec/data/plan/plugins/**/*.yaml'].each do |plan_file|
-    describe "plugin tests from: '#{plan_file}'" do
-      Dopi.configuration.ssh_pass_auth = true
-      Dopi.configuration.mco_config = 'spec/data/mco/client.cfg'
-      plan_parser = DopCommon::Plan.new(YAML.load_file(plan_file))
-      plan = Dopi::Plan.new(plan_parser, 'fakeid')
-      plan.steps.each do |step|
-        it "successfully runs the step: '#{step.name}'" do
-          step.run(plan.max_in_flight)
-          expect(step.state_done?).to be true
-        end
-      end
-    end
-  end
-
 end
