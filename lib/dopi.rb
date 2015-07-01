c95e53cbbe96008bbaa0be45abcb04da84179d68
Andreas Zuber <zuber@puzzle.ch>
2015-07-01 16:33:53 +0200

    Move some methods to Dopi module from executable

--- lib/dopi.rb
+++ lib/dopi.rb
@@ -13,8 +13,36 @@ require "dopi/version"
 module Dopi
   class << self
 
+    def plan_valid?(plan_file)
+      plan_parser = DopCommon::Plan.new(YAML.load_file(plan_file))
+      plan = Dopi::Plan.new(plan_parser)
+      plan.valid?
+    end
+
+    def add_plan(plan_file)
+      raise StandardError, 'Plan not valid; did not add' unless plan_valid?(plan_file)
+      plan_name = plan_cache.add(plan_file)
+      load_plan(plan_name)
+    end
+
     def load_plan(plan_name)
       plan_exists?(plan_name) ? YAML::load(File.read(dump_file(plan_name))) : create(plan_name)
+    end
+
+    def run_plan(plan)
+      begin
+        run_thread    = Thread.new { plan.run }
+        update_thread = Thread.new do
+          until plan.state_done? or plan.state_failed? do
+            Dopi.save_plan(plan) if plan.state_changed?
+          end
+          sleep(1)
+        end
+        run_thread.join
+        update_thread.join
+      ensure
+        Dopi.save_plan(plan)
+      end
     end
 
     def save_plan(plan)
