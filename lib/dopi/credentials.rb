667972ef00d68cd4a8f10a693495ff8f6dff3a20
Andreas Zuber <zuber@puzzle.ch>
2015-07-29 17:42:38 +0200

    Add checks and validation for supported credential types per plugins

--- lib/dopi/credentials.rb
+++ lib/dopi/credentials.rb
@@ -1,3 +1,15 @@
+#
+# Dopi Credentials helper module
+#
+# This module will provide a credentials method which contains all the credential
+# objects for the plugin.
+#
+# Make sure you call "validate_credentials" method from your validation method.
+#
+# Implement a method "supported_credential_types" in your plugin if you want to limit
+# the types which are supported and trow an error during validation if some other type
+# is assigned
+#
 module Dopi
   module Credentials
     include DopCommon::Validator
@@ -19,16 +31,15 @@ module Dopi
       return false if hash[:credentials].nil? # credentials is optional
       hash[:credentials].kind_of?(String) or hash[:credentials].kind_of?(Array) or
         raise CommandParsingError, "the value for 'credentials' has to be a string or an array of strings"
-      if hash[:credentials].kind_of?(String)
-        @step.plan.credentials.has_key?(hash[:credentials]) or
-          raise CommandParsingError, "Credentials #{hash[:credentials]} are not configured"
-      end
-      if hash[:credentials].kind_of?(Array)
-        hash[:credentials].each do |c|
-          c.kind_of?(String) or
-            raise CommandParsingError, "All values in the 'credentials' array have to be strings"
-          @step.plan.credentials.has_key?(c) or
-            raise CommandParsingError, "Credentials #{c} are not configured"
+      [hash[:credentials]].flatten.each do |c|
+        c.kind_of?(String) or
+          raise CommandParsingError, "All values in the 'credentials' array have to be strings"
+        @step.plan.credentials.has_key?(c) or
+          raise CommandParsingError, "Credentials #{c} are not configured"
+        if self.methods.include?(:supported_credential_types)
+          cred_type = @step.plan.credentials[c].type
+          supported_credential_types.include?(cred_type) or
+            raise CommandParsingError, "Credential #{c} is of type #{cred_type}, which is not supported by the plugin"
         end
       end
       true
--- spec/unit/dopi/credentials_spec.rb
+++ spec/unit/dopi/credentials_spec.rb
@@ -3,16 +3,19 @@ require 'spec_helper'
 class CredentialsTestKlass
   include Dopi::Credentials
   attr_accessor :hash, :step
+  def supported_credential_types() [:username_password] end
 end
 
 describe Dopi::Credentials do
 
   before :each do
     @credentials = CredentialsTestKlass.new
-    @fake_credentials = instance_double('DopCommon::Credentials')
+    @fake_credentials  = instance_double('DopCommon::Credentials', :type => :username_password)
+    @fake_credentials4 = instance_double('DopCommon::Credentials', :type => :non_existing_type)
     plan = instance_double('Dopi::Plan', :credentials => {
       'test_credentials_01' => @fake_credentials,
-      'test_credentials_02' => @fake_credentials
+      'test_credentials_02' => @fake_credentials,
+      'test_credentials_04' => @fake_credentials4
     })
     @credentials.step = instance_double('Dopi::Step', :plan => plan)
   end
@@ -38,6 +41,10 @@ describe Dopi::Credentials do
     end
     it 'raises an error if the credentials do not exist' do
       @credentials.hash = {:credentials => ['test_credentials_01', 'test_credentials_03']}
+      expect{@credentials.credentials}.to raise_error Dopi::CommandParsingError
+    end
+    it 'raises an error if one of the credential types is not supported' do
+      @credentials.hash = {:credentials => ['test_credentials_01', 'test_credentials_04']}
       expect{@credentials.credentials}.to raise_error Dopi::CommandParsingError
     end
   end
