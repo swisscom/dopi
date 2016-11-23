662f34597d177cee6c07ae404e06dcee108e2049
Andreas Zuber <zuber@puzzle.ch>
2016-11-23 11:44:14 +0100

    replace popen3 with process spawn and attach a proper pty insted of a pipe as stdin to avoid the tcgetattr errors on some systems

--- lib/dopi/connector/local.rb
+++ lib/dopi/connector/local.rb
@@ -3,6 +3,7 @@
 # machine. It does all the signal handling and makes
 # sure processes are stopped or killed.
 #
+require 'pty'
 require 'open3'
 
 module Dopi
@@ -12,38 +13,53 @@ module Dopi
       # The command method executes the command of the step.
       # Returns an array with stdio, sterror and exit code.
       def local_command(env, command_string)
+        master, slave = PTY.open
+        stdout_r, stdout_w = IO.pipe
+        stderr_r, stderr_w = IO.pipe
         cmd_stdout = ''
         cmd_stderr = ''
+        options = {
+          :pgroup          => true,
+          :unsetenv_others => true,
+          :in              => slave,
+          :out             => stdout_w,
+          :err             => stderr_w,
+        }
         log(:debug, "Executing #{command_string} for command #{name}")
         log(:debug, "Environment: #{env.to_s}")
-        cmd_exit_code = Open3.popen3(merged_env(env), command_string, :pgroup => true, :unsetenv_others => true) do |stdin, stdout, stderr, wait_thr|
-          signal_handler = Proc.new do |signal|
-            case signal
-            when :abort then Process.kill(:TERM, wait_thr.pid)
-            when :kill  then Process.kill(:KILL, wait_thr.pid)
-            end
+
+        pid = Process.spawn(merged_env(env), command_string, options)
+        slave.close
+        stdout_w.close
+        stderr_w.close
+
+        signal_handler = Proc.new do |signal|
+          case signal
+          when :abort then Process.kill(:TERM, pid)
+          when :kill  then Process.kill(:KILL, pid)
           end
-          on_signal(signal_handler)
-          stdin.close
-          stdout_thread = Thread.new do
-            until ( line = stdout.gets ).nil? do
-              cmd_stdout << line
-              log(:debug, line.gsub("\n", '').gsub("\r", ''))
-            end
-          end
-          stderr_thread = Thread.new do
-            until ( line = stderr.gets ).nil? do
-              cmd_stderr << line
-              log(:error, line.gsub("\n", '').gsub("\r", ''))
-            end
-          end
-          exit_status = wait_thr.value
-          stdout_thread.join
-          stderr_thread.join
-          delete_on_signal(signal_handler)
-          exit_status
         end
-        [ cmd_stdout, cmd_stderr, cmd_exit_code.exitstatus ]
+
+        on_signal(signal_handler)
+        stdout_thread = Thread.new do
+          until ( line = stdout_r.gets ).nil? do
+            cmd_stdout << line
+            log(:debug, line.gsub("\n", '').gsub("\r", ''))
+          end
+        end
+
+        stderr_thread = Thread.new do
+          until ( line = stderr_r.gets ).nil? do
+            cmd_stderr << line
+            log(:error, line.gsub("\n", '').gsub("\r", ''))
+          end
+        end
+
+        _, status = Process.wait2(pid)
+        stdout_thread.join
+        stderr_thread.join
+        delete_on_signal(signal_handler)
+        [ cmd_stdout, cmd_stderr, status.exitstatus ]
       end
 
     private
