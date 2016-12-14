43b5eb7b3c64494a16bd105342e13289a8ac8318
Andreas Zuber <zuber@puzzle.ch>
2016-12-14 13:59:11 +0100

    add update flag to add function so it is always possible to add even if the plan already exists

--- lib/dopi/cli/command_add.rb
+++ lib/dopi/cli/command_add.rb
@@ -7,12 +7,23 @@ module Dopi
         desc 'Add a new plan file to the plan cache'
         arg_name 'plan_file'
         command :add do |c|
+          c.desc 'update the plan if it already exists'
+          c.default_value false
+          c.switch [:update, :u]
+
           c.action do |global_options,options,args|
             help_now!('Specify a plan file to add') if args.empty?
             help_now!('You can only add one plan') if args.length > 1
             plan_file = args[0]
-            plan_name = Dopi.add(plan_file)
-            puts plan_name
+            begin
+              puts Dopi.add(plan_file)
+            rescue DopCommon::PlanExistsError => e
+              if options[:update]
+                puts Dopi.update_plan(plan_file, {})
+              else
+                raise e
+              end
+						end
           end
         end
 
